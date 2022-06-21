5530b2936f6659a51cd98d6571bd685c33dac4f7
DrMe3za <101528212+DrMe3za@users.noreply.github.com>
2022-06-22 00:59:50 +0200

    MyFunction
    7atetloko function fashe5a nek aho

--- hub.cpp
+++ hub.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 #include <cmath>
 #include <string>
-
 using namespace std;
-
+string DrMe3za() {
+	return "ana aho ya regala" ;
+}
 string mo2asMasseage(){
 	return "I made a Function!!!";
 }
-
 string mostasMassage() {
 
 	return "mo2a pls make a function!";
 }
-
 int main() {
-	
 	cout << mostasMassage();
 	cout << mo2asMasseage();
-
+	cout << DrMe3za() ;
 	return 0;
 }
