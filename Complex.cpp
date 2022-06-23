519c364065c7c9e9ed505e04a0cd323cee866e75
MostafaVT <58429502+MostafaVT@users.noreply.github.com>
2022-06-23 08:09:56 +0200

    Major Changes !!
    Now by adding the actual over loaded constructors we can manipulate the values and the attributes for each object and i've added display method
    better than the previous one and disabling the user inputs from the terminal as for testing and debugging the code more efficiently ,lastly
    adding some "Setters" and "Getters" will help in the future to manipulating the values of the objects.

--- Complex.cpp
+++ Complex.cpp
@@ -1,37 +1,71 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+
 using namespace std;
 
-class Complex{
+// Complex numbers class generator
+class Complex{                                               
     public:
-        int realPart;
-        int imgPart;
-
-        
-        Complex(int x,int y){
+        // Specifc vars. for each object will be created
+        int realPart;                                      
+        int imgPart;                                                                    
+        // Constructors for initailzing the vars. of the object
+        Complex(){                              
+            realPart = 0;
+            imgPart = 0;
+        }
+        Complex(int x){                              
+            realPart = x;
+            imgPart = 0;
+        }
+        Complex(int x,int y){                              
             realPart = x;
             imgPart = y;
         }
+        // "Getters"
+        int getReal(){                        
+            return realPart;
+        }
+        int getImgOf(){
+            return imgPart;
+        }
+        // "Setters"
+        void setReal(int x ,Complex com){                   
+            com.realPart = x;
+        }
+        void setImg(int x ,Complex com){
+            com.imgPart = x;
+        }
+        // Displaying 
+        void display(){                         
+            cout << to_string(realPart)+" + " + to_string(imgPart)+"i " <<endl;
+        }
+
 };
-
-string printComplex(int x,int y){
-    return x+" "+ y+'i';
-}
-
 int main(){
-    int input1,intput2;
-    cout << "please,enter the real part : ";
+
+    // User_input_prompt  [Disabled Temporary!]
+    /*
+    int input1,intput2;                                   
+    cout << "Enter the realPart: ";
     cin >> input1;
-    cout << "please,enter the imaginary part : ";
+    cout << "Enter the imaginaryPart: ";
     cin >> intput2;
+    */
 
-    Complex complex1(input1,intput2);
+    // Creating new instance and sending values to the object instance attribute
+    Complex Mycomplex1(5,3);
+    Complex Mycomplex2(0,0);
+    Complex Mycomplex3(73);
 
-    string out = "complex1.realPart++complex1.imgPart";
+    // ____________________TESTING____________________
 
-    cout << complex1.realPart ;
+    Mycomplex1.display();
+    Mycomplex2.display();
+    Mycomplex3.display();
     
-
+    cout << Mycomplex1.getImgOf()<<endl;
+    
     return 0;
 }
